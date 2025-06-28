51c6f294216ce8c58f995aef1a906ae4aeeec12b
softlee144 <ockjungbong@gmail.com>
2025-06-28 16:53:06 +0900

    [프론트] 로그아웃 기능 추가

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,13 +1,45 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
+  const navigate = useNavigate();
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_API_URL}/api/hello`)
       .then((response) => console.log(response.data));
   }, []);
-  return <div>LandingPage</div>;
+
+  const onClickHandler = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/api/users/logout`, {
+        withCredentials: true, // 쿠키 포함 요청!
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.data.success) {
+          navigate("/login");
+        } else {
+          alert("로그아웃 하는데 실패 했습니다.");
+        }
+      });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>로그아웃</button>
+    </div>
+  );
 }
 
 export default LandingPage;
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./_reducers";
 import App from "./App";
+import axios from "axios";
+axios.defaults.withCredentials = true; // 모든 요청에 쿠키 포함되도록 설정
 
 const store = configureStore({
   reducer: rootReducer,
